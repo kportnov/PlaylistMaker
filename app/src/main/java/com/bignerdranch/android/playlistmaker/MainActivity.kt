34f16c12876bb89df27c9d0b47e757ec7ba7804a
KPortnov <pilot@kportnov.ru>
2025-08-06 18:24:17 +0300

    Home task sprint number 8
    С исправлениями после review

--- app/src/main/java/com/bignerdranch/android/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/playlistmaker/MainActivity.kt
@@ -2,10 +2,7 @@ package com.bignerdranch.android.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.View.OnClickListener
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -31,43 +28,5 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, MediaLibraryActivity::class.java)
             startActivity(intent)
         }
-
-
-/*
-        //Домашнее задание с тостами>>>
-
-        val buttonListener = object : OnClickListener {
-            override fun onClick(p0: View?) {
-                showToast(true, p0)
-            }
-        }
-
-        //через анонимные классы объеденил в один слушатель
-        buttonSearch.setOnClickListener(buttonListener)
-        buttonMediaLibrary.setOnClickListener(buttonListener)
-        buttonSettings.setOnClickListener(buttonListener)
-
-
-        //через лямбда
-        buttonSearch.setOnClickListener { showToast(false, it) }
-        buttonMediaLibrary.setOnClickListener { showToast(false, it) }
-        buttonSettings.setOnClickListener { showToast(false, it) }*/
-
     }
-
-/*    fun showToast(isAnonymous: Boolean, p0: View?) {
-        val searchText1 = "Button \"Search\" has been clicked and anonymous class works"
-        val searchText2 = "Button \"Search\" has been clicked and lyambda works"
-        val mediaText1 = "Button \"Media room\" has been clicked and anonymous class works"
-        val mediaText2 = "Button \"Media room\" has been clicked and lyambda works"
-        val settingsText1 = "Button \"Settings\" has been clicked and anonymous class works"
-        val settingsText2 = "Button \"Settings\" has been clicked and lyambda works"
-        val text = when (p0?.id) {
-            R.id.button_search -> if (isAnonymous) searchText1 else searchText2
-            R.id.button_media_library -> if (isAnonymous) mediaText1 else mediaText2
-            R.id.button_settings -> if (isAnonymous) settingsText1 else settingsText2
-            else -> "ERROR"
-        }
-        Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
-    }*/
 }
